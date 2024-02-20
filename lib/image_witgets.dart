5edc07c55a7e89977e18ca8ee6921f7be3f943a9
Burak Turker <pythonu0@gmail.com>
2024-02-20 18:03:10 +0300

    fadeimages,placeholder gibi kavramlar bitti.

--- lib/image_witgets.dart
+++ lib/image_witgets.dart
@@ -11,32 +11,60 @@ class ImageOrnekleri extends StatelessWidget {
     String _imgUrl2 ='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.UM1sd7YomSksjeRdSXC8lAHaFj%26pid%3DApi&f=1&ipt=1196059b283742025be664ac1b3a5b7a7d17b5d40b19fdd93b401d287f238a75&ipo=images';
     return Center(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            color: Colors.lightBlue,
-            width: 150,
-            height: 150,
-            child: Image.asset(
-              'assets/images/f4.jpg',
-              fit: BoxFit.cover,)
-              ),
-
-            Container(
-            color: Colors.lightBlue,
-            width: 150,
-            height: 150,
-            child: Image.network(_imgURL)
-
-            ),Container(
-              color: Colors.white,
-            child: CircleAvatar(
-              //child: Text('KJ',style: Theme.of(context).textTheme.headlineMedium),//hazır tema kullanımı
-              backgroundImage: NetworkImage(_imgUrl2,),
-              backgroundColor: Colors.amber,
-              foregroundColor: Colors.grey,
-              radius: 80,
+          IntrinsicHeight(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+            
+            Expanded(
+              child: Container(
+                color: Colors.lightBlue,
+                child: Image.asset(
+                  'assets/images/f4.jpg',
+                  fit: BoxFit.cover,)
+                  ),
             ),
+            
+            Expanded(
+              child: Container(
+                color: Colors.lightBlue,
+                child: Image.network(_imgURL,fit: BoxFit.cover,)
+                ),
             ),
+            
+            Expanded(
+              child: Container(
+                  color: Colors.white,
+                child: CircleAvatar(
+                  //child: Text('KJ',style: Theme.of(context).textTheme.headlineMedium),//hazır tema kullanımı
+                  backgroundImage: NetworkImage(_imgUrl2,),
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.grey,
+                ),
+                ),
+            ),
+              ],
+            ),
+          ),  
+       
+       Expanded(
+         child: Container(
+          margin: EdgeInsets.all(20),
+          height: 200,
+          child: 
+          FadeInImage.assetNetwork(
+            placeholder: 'assets/images/loading.gif', 
+            image: _imgURL,
+            fit: BoxFit.cover,
+            )
+          ),
+        ),
+        Expanded(
+          child: Placeholder(),
+          )
+
         ],
       ),
     );
